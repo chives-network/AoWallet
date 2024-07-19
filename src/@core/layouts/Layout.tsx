05d3180e393b1c9ecc28cddf204a6703b9c2aab9
ChatBookAI <reactchives@gmail.com>
2024-07-19 19:03:54 +0800

    Clear Code

--- src/@core/layouts/Layout.tsx
+++ src/@core/layouts/Layout.tsx
@@ -6,7 +6,6 @@ import { LayoutProps } from 'src/@core/layouts/types'
 
 // ** Layout Components
 import VerticalLayout from './VerticalLayout'
-import HorizontalLayout from './HorizontalLayout'
 
 const Layout = (props: LayoutProps) => {
   // ** Props
@@ -34,10 +33,6 @@ const Layout = (props: LayoutProps) => {
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [hidden])
-
-  if (settings.layout === 'horizontal') {
-    return <HorizontalLayout {...props}>{children}</HorizontalLayout>
-  }
 
   return <VerticalLayout {...props}>{children}</VerticalLayout>
 }
