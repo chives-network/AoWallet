94ca3d10812998789556052287908f61c4e5c5b8
ChatBookAI <reactchives@gmail.com>
2024-07-19 20:16:10 +0800

    clear system

--- next.config.cjs
+++ next.config.cjs
@@ -10,8 +10,7 @@ module.exports = {
   reactStrictMode: false,
   webpack: config => {
     config.resolve.alias = {
-      ...config.resolve.alias,
-      apexcharts: path.resolve(__dirname, './node_modules/apexcharts-clevision')
+      ...config.resolve.alias
     }
 
     return config
