0986c39e2e537ece117cfe187a385c1e2ce40aba
ChatBookAI <reactchives@gmail.com>
2024-07-26 12:24:14 +0800

    "next": "^14.2.5",

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.aowallet',
+  appName: 'com.aowallet',
+  webDir: 'out'
+};
+
+export default config;
--- next.config.cjs
+++ next.config.cjs
@@ -1,16 +1,18 @@
-import path from 'path';
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path')
 
 /** @type {import('next').NextConfig} */
 
 // Remove this if you're not using Fullcalendar features
 
 module.exports = {
-  output: "export",
+  output: 'export',
   trailingSlash: true,
   reactStrictMode: false,
   webpack: config => {
     config.resolve.alias = {
-      ...config.resolve.alias
+      ...config.resolve.alias,
+      apexcharts: path.resolve(__dirname, './node_modules/apexcharts-clevision')
     }
 
     return config
