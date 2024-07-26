56fee61c79ef218fe0804f2a3b3842b1bd90f1ed
ChatBookAI <reactchives@gmail.com>
2024-07-26 12:35:25 +0800

    AoWallet

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.aowallet',
-  appName: 'com.aowallet',
+  appName: 'AoWallet',
   webDir: 'out'
 };
 
