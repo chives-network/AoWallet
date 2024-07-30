29ca5e1f654e534d0c08039bd8a70b65b0a5c7cb
ChatBookAI <reactchives@gmail.com>
2024-07-30 19:26:54 +0800

    bundledWebRuntime

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,8 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.aowallet.app',
   appName: 'AoWallet',
-  webDir: 'out'
+  webDir: 'out',
+  "bundledWebRuntime": true
 };
 
 export default config;
