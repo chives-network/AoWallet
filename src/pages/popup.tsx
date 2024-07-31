1334c46a61e320674a740a5be05e1943c3d1a00f
ChatBookAI <reactchives@gmail.com>
2024-07-31 13:20:55 +0800

    web_accessible_resources

---
+++ src/pages/popup.tsx
@@ -0,0 +1,13 @@
+import HomeModel from '../views/Home/Home';
+
+const HomeModelApp = () => {
+
+
+  return <HomeModel />
+
+}
+
+//HomeModelApp.contentHeightFixed = true
+
+export default HomeModelApp
+
