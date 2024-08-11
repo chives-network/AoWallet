40662d42bd868a0b0986d29b0897e8ea306f8d95
ChatBookAI <reactchives@gmail.com>
2024-08-11 09:41:42 +0800

    Content-script

--- public/content-script.js
+++ public/content-script.js
@@ -1,10 +1,48 @@
 
+window.addEventListener('message', function(event) {
+  if (event.source !== window) return;
+  const callID = event.data.callID
+  const ext = event.data.ext
+  const type = event.data.type
+  const origin = event.data.origin
+  const isTrusted = event.data.isTrusted
+  const permissions = (event.data.data && event.data.data.params && event.data.data.params[0]) ?? [];
+  const name = (event.data.data && event.data.data.params && event.data.data.params[1]) ?? 'AoWalletExample';
+  console.log("type", type, permissions, ext)
+  if (type && (type === 'api_connect')) {
+    chrome.runtime.sendMessage({ action: "Open_Popup" }, (response) => {
+      console.log("Extension Open_Popup is open:", response);
+    });
+  }
+
+});
+
+
+
+window.addEventListener('message', function(event) {
+  if (event.source !== window) return;
+  if (event.data.type && (event.data.type === 'connect_result')) {
+      console.log('connect_result');
+  }
+});
+
+window.addEventListener('message', function(event) {
+  if (event.source !== window) return;
+  if (event.data.type && (event.data.type === 'arconnect_event')) {
+      console.log('arconnect_event');
+  }
+});
+
+
+
+
+
 // Page request to open popup
 window.addEventListener('message', function(event) {
   if (event.source !== window) return;
   if (event.data.type && (event.data.type === 'Chrome_Open_Extension_defmalnpgnhpcoaadaohehpdaodpfjla')) {
       console.log('Content script received message to open extension popup');
-      chrome.runtime.sendMessage({ action: 'Open_Popup' });
+      //chrome.runtime.sendMessage({ action: 'Open_Popup' });
   }
 });
 
--- public/service-worker.js
+++ public/service-worker.js
@@ -2,8 +2,8 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if (request.action === 'Open_Popup') {
       console.log('Background script received message to open popup');
-
       chrome.action.openPopup();
+      sendResponse({ openPopup: true });
   }
 });
 
