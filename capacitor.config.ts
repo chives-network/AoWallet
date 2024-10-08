4359b3c06816b31fe8ad40c54175fbd8b4d30148
Chives Network <reactchives@gmail.com>
2024-10-07 20:17:59 -0700

    org.aowallet

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.aowallet.app',
+  appId: 'org.aowallet.app',
   appName: 'AoWallet',
   webDir: 'out',
   "bundledWebRuntime": true
